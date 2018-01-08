2c14f8792787d89fdd094a607a79edaf08937f7d
Lynn Kirste <estrumpswan@gmail.com>
2018-01-07 18:24:21 -0700

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/Http';
 
 
 import { AppComponent } from './app.component';
+import { ValueComponent } from './Value/Value.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    ValueComponent
+],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
